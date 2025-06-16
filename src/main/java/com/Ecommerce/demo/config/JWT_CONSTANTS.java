cf35fbde71a3fbde73b13b86950a6270aa5cb2f4
yogita makkar <yogitamakkar17@gmail.com>
2025-06-16 23:13:15 +0530

    Removed JWT_CONSTANTS.java to protect secret key

--- src/main/java/com/Ecommerce/demo/config/JWT_CONSTANTS.java
+++
@@ -1,7 +0,0 @@
-package com.Ecommerce.demo.config;
-
-public class JWT_CONSTANTS {
-    public static final String SECRET_KEY = "dsfsofhsgohighrigherigherigheigherigero";
-    public static final String JWT_HEADER = "Authorization";
-    public static final String TOKEN_PREFIX = "Bearer ";
-}
